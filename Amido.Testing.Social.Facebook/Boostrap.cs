a518930770ac126e67fe765a9a50471aec888b2b
seanmcalinden <sean.f.mcalinden@live.co.uk>
2014-07-14 20:55:48 +0100

    Added bootstrap and updated nuget

---
+++ Amido.Testing.Social.Facebook/Boostrap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Amido.Testing.Social.Facebook.PageObjects;
+
+using OpenQA.Selenium;
+
+namespace Amido.Testing.Social.Facebook
+{
+    public static class Bootstrap
+    {
+        public static void Init(Func<IWebDriver> webDriver)
+        {
+            FacebookSignInPageObject.Init(webDriver);
+            FacebookAllowAppPageObject.Init(webDriver);
+        }
+    }
+}
---
+++ Amido.Testing.Social.Facebook/PageObjects/FacebookAllowAppPageObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Amido.WebDriver.Utilities.Extensions;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Amido.Testing.Social.Facebook.PageObjects
+{
+    public class FacebookAllowAppPageObject
+    {
+        private static Func<IWebDriver> webDriver;
+
+        public static void Init(Func<IWebDriver> getWebDriver)
+        {
+            webDriver = getWebDriver;
+        }
+
+        public FacebookAllowAppPageObject()
+        {
+            if (webDriver == null)
+            {
+                throw new Exception("You must call FacebookAllowAppPageObject.Init once before using this class");
+            }
+
+            PageFactory.InitElements(webDriver(), this);
+        }
+
+        [FindsBy(How = How.Name, Using = "__CONFIRM__")]
+        public IWebElement SignInAllow { get; set; }
+
+        public bool IsLoaded()
+        {
+            return this.SignInAllow.WaitUntil(e => e.Displayed).Displayed;
+        }
+    }
+}
